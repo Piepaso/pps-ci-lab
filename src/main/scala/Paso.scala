021c273186032af32b080555b122ed0960a10684
Pietro Pasini - pietro.pasini4@studio.unibo.it <pietro.pasini4@studio.unibo.it>
2026-04-20 11:57:07 +0200

    add main Paso

--- src/main/scala/Paso.scala
+++ src/main/scala/Paso.scala
@@ -1,2 +1,9 @@
-import java/Paso
+import noi.*
+
+@main def chimaTutti(): Unit = {
+  Paso().saludo()
+  Fedex("fede", "brighi").printHello()
+  Paggetti().getName()
+  Sapo2k3("Mat", "Sapo").getSurname()
+}
 
